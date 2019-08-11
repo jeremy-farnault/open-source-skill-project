da6305dafd86a30c4878a993ac1fe97a8bfb6208
Jeremy Farnault <haskkor@gmail.com>
2019-08-11 18:48:53 +1200

    Added all the packages

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,22 +1,23 @@
 import { ApolloProvider } from '@apollo/react-hooks'
+import ApolloClient from 'apollo-boost'
 import React, { memo } from 'react'
 import { Provider } from 'react-redux'
-import { Filters, MainSection } from '..'
 import { configureStore } from '../../core/configureStore'
 import { initialState } from '../../Interfaces/store'
-import { portfolioClient } from '../../utils/apolloClients'
+import { config } from '../../config'
 import { AppContainer } from './AppStyled'
 
 const store = configureStore(initialState)
 
+export const client = new ApolloClient({
+  uri: config.apolloClient
+})
+
 export const App = memo(() => {
   return (
     <Provider store={store} key={'provider'}>
-      <ApolloProvider client={portfolioClient}>
-        <AppContainer>
-          <MainSection />
-          <Filters />
-        </AppContainer>
+      <ApolloProvider client={client}>
+        <AppContainer></AppContainer>
       </ApolloProvider>
     </Provider>
   )
